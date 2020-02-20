8d60e993427eeef18c42a0ad8957aa8a7e196125
Sahan Chandrabahu <xnscb8.chandrabahu@gmail.com>
2020-02-20 20:22:53 +0530

    toilet.py

---
+++ toilet.py
@@ -0,0 +1,10 @@
+print("This is a toilet game")
+choice = bool(input("Do you want to go to toilet: "))
+if(choice == True):
+    choice2 = bool(input("Do you want to put number 1: "))
+    if(choice == True):
+        print("----------------------")
+    else:
+        print("0000----00000---0000-----000000")
+else:
+    print("You can wash your face or go out of toilet :-)")
