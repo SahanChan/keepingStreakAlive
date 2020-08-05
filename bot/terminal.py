2e49097ae33e2685b6178eb66afde2685b55ef72
sahan <xnscb8.chandrabahu@gmail.com>
2020-08-05 16:33:20 +0530

    Bot_commit

--- bot/terminal.py
+++ bot/terminal.py
@@ -1,12 +1,21 @@
 import os
-import math
+import random
+from time import strftime
+
+x = random.randint(5, 10)
 
 
-os.system('git add .')
-os.system("git commit -m 'Bot_commit'")
-os.system("git push -u origin master")
+for i in range(0, x):
 
-# CUR_DIR = os.getcwd()
-# my_file = open()
-# my_file.write(CUR_DIR)
-# # CUR_DIR =
+    my_file = open("DateAndTime_Commit.dat", 'a')
+
+    my_file.write(str(strftime("%Y-%m-%d")))
+    my_file.write(" - ")
+    my_file.write(str(strftime("%H:%M:%S")))
+    my_file.write("\t")
+    my_file.write("Made a Commit")
+    my_file.write("\n")
+    my_file.close()
+    os.system('git add .')
+    os.system("git commit -m 'Bot_commit'")
+    os.system("git push -u origin master")
