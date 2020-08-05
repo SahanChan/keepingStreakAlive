ce01a333f9bdd9e62405d267d7c1655dda214c68
sahan <xnscb8.chandrabahu@gmail.com>
2020-08-05 15:54:59 +0530

    Bot_commit

---
+++ bot/terminal.py
@@ -0,0 +1,12 @@
+import os
+import math
+
+
+os.system('git add .')
+os.system("git commit -m 'Bot_commit'")
+os.system("git push -u origin master")
+
+# CUR_DIR = os.getcwd()
+# my_file = open()
+# my_file.write(CUR_DIR)
+# # CUR_DIR =
